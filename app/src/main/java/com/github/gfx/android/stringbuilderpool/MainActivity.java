1b83d205ec2ad888e69fa726e7eb8260b42d7ed5
FUJI Goro (gfx) <goro-fuji@cookpad.com>
2016-02-23 12:03:14 +0900

    initial commit

---
+++ app/src/main/java/com/github/gfx/android/stringbuilderpool/MainActivity.java
@@ -0,0 +1,100 @@
+package com.github.gfx.android.stringbuilderpool;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.util.Pools;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    int N = 100000;
+
+    Pools.SimplePool<StringBuilder> simplePool = new Pools.SimplePool<>(10);
+
+    Pools.SynchronizedPool<StringBuilder> synchronizedPool = new Pools.SynchronizedPool<>(10);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        AsyncTask.SERIAL_EXECUTOR.execute(new Runnable() {
+            @Override
+            public void run() {
+                f();
+            }
+        });
+    }
+
+    void f() {
+        System.gc();
+        long t0 = System.currentTimeMillis();
+        for (int i = 0; i < N; ++i) {
+            StringBuilder sb = obtain(simplePool);
+
+            sb.append('"');
+            sb.append("foo");
+            sb.append('"');
+            sb.append('.');
+            sb.append('"');
+            sb.append("foo");
+            sb.append('"');
+
+            simplePool.release(sb);
+        }
+        Log.d("XXX", "SimplePool: " + (System.currentTimeMillis() - t0) + "ms");
+
+        System.gc();
+        t0 = System.currentTimeMillis();
+        for (int i = 0; i < N; ++i) {
+            StringBuilder sb = obtain(synchronizedPool);
+
+            sb.append('"');
+            sb.append("foo");
+            sb.append('"');
+            sb.append('.');
+            sb.append('"');
+            sb.append("foo");
+            sb.append('"');
+
+            synchronizedPool.release(sb);
+        }
+        Log.d("XXX", "SynchronizedPool: " + (System.currentTimeMillis() - t0) + "ms");
+
+        System.gc();
+        t0 = System.currentTimeMillis();
+        for (int i = 0; i < N; ++i) {
+            StringBuilder sb = nonPoolObtain();
+
+            sb.append('"');
+            sb.append("foo");
+            sb.append('"');
+            sb.append('.');
+            sb.append('"');
+            sb.append("foo");
+            sb.append('"');
+        }
+        Log.d("XXX", "No pool: " + (System.currentTimeMillis() - t0) + "ms");
+    }
+
+    @NonNull
+    static StringBuilder nonPoolObtain() {
+        return new StringBuilder();
+    }
+
+    @NonNull
+    static StringBuilder obtain(Pools.Pool<StringBuilder> pool) {
+        StringBuilder sb = pool.acquire();
+        if (sb == null) {
+            return new StringBuilder();
+        }
+        return sb;
+    }
+}
